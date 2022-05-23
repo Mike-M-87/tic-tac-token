af133e428b6bb581570859af4196d4aff50b310b
Mike-M-87 <micminn87@gmail.com>
2022-05-23 13:51:37 +0300

    meta

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <meta charSet="utf-8"></meta>
       <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossOrigin="anonymous"></link>
       <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossOrigin="anonymous"></Script>
